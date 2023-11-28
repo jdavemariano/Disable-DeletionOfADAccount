909588eebc36ca82d8b37e62d79947c1a3a1d33d
jdavemariano <143034233+jdavemariano@users.noreply.github.com>
2023-11-29 03:43:37 +0800

    Update delete-logupload.py

--- delete-logupload.py
+++ delete-logupload.py
@@ -1,1 +1,20 @@
+import boto3
+from datetime import datetime
 
+def upload_file_to_s3(file_path, bucket_name):
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name, file_extension = file_path.split('.')
+    new_file_name = f"{file_name}_{timestamp}.{file_extension}"
+
+    s3 = boto3.client('s3')
+    try:
+        # Upload the file with the new name to S3
+        s3.upload_file(file_path, bucket_name, new_file_name)
+        print(f"Log File uploaded successfully with timestamp: {timestamp}")
+    except Exception as e:
+        print(f"Error uploading file to S3: {e}")
+
+if __name__ == "__main__":
+    bucket_name = "ad-user-disable-logs"
+    file_path = "Disabled_AD_Account.txt"
+    upload_file_to_s3(file_path, bucket_name)
