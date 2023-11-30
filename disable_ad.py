a94b154527c11b29b90f4003c7f7767dfed5828a
jdavemariano <143034233+jdavemariano@users.noreply.github.com>
2023-11-30 22:43:46 +0800

    Update disable_ad.py

--- disable_ad.py
+++ disable_ad.py
@@ -4,29 +4,29 @@ import os
 import sys 
 
 domain = sys.argv[1]
-sourceou = sys.argv[2]
-destinationou = sys.argv[3]
+
+# Read JSON file
+with open("domain_accounts.json") as json_file:
+    config = json.load(json_file)
+    instance_info = config.get(domain)
+
+if not instance_info:
+    print(f"Domain '{domain}' not found in configuration.")
+    sys.exit(1)
+
+target_instance = instance_info["target_instance"]
+sourceou = instance_info["sourceou"]
+destinationou = instance_info["destinationou"]
 
 ssm_file = open("disable_account.json")
 ssm_json = ssm_file.read()
-
-instance_ids = {
-	"deltekdev":"i-04d0e953afe07b3a3",
-	"costpoint":"i-0e82a12d1ef934425",
-  "dco":"i-0fe3ff3ff41c18b17",
-	"flexplus":"i-0f2717bceb18eea6f",
-	"globaloss":"i-04b225ae477c52288",
-	"engdeltek":"i-0667aa10a44eafc7c",
-}
-
-target_domain = instance_ids[domain]
 
 ssm_doc_name = 'disable_account'
 ssm_client = boto3.client('ssm', region_name="us-east-1")
 
 ssm_create_response = ssm_client.create_document(Content = ssm_json, Name = ssm_doc_name, DocumentType = 'Command', DocumentFormat = 'JSON', TargetType =  "/AWS::EC2::Instance")
 
-ssm_run_response = ssm_client.send_command(InstanceIds = [target_domain], DocumentName=ssm_doc_name, DocumentVersion="$DEFAULT", TimeoutSeconds=120,  Parameters={'destinationOU':[destinationou],'sourceOU':[sourceou],})
+ssm_run_response = ssm_client.send_command(InstanceIds = [target_instance], DocumentName=ssm_doc_name, DocumentVersion="$DEFAULT", TimeoutSeconds=120,  Parameters={'destinationOU':[destinationou],'sourceOU':[sourceou],})
 print(f'{ssm_run_response}\n')
 cmd_id = ssm_run_response['Command']['CommandId']
 
